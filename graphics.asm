e1c80569bf708827b3a3fd2f09f6c2546f9ec933
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2014-03-29 11:24:32 -0400

    refactored graphics generator

--- graphics.asm
+++ graphics.asm
@@ -7,7 +7,6 @@
 ; Atari's pixel ratio (approx. 1.66:1)
 
 Tiles:
-
     .BYTE %00000000
     .BYTE %00000000
     .BYTE %00000000
@@ -112,7 +111,7 @@ Tiles:
     .BYTE %00100100
     .BYTE %10101101
     .BYTE %10101101
-    .BYTE %00100101
+    .BYTE %00100100
     .BYTE %11111111
     .BYTE %11111111
 
@@ -175,8 +174,8 @@ Tiles:
     .BYTE %11000101
     .BYTE %11010101
     .BYTE %11000101
-TitleScreenTiles:
 
+TitleScreenTiles:
     .BYTE %11111111
     .BYTE %11000011
     .BYTE %10011001
@@ -224,65 +223,65 @@ TitleScreenTiles:
     .BYTE %10101011
     .BYTE %11011001
     .BYTE %11111111
-Digits
-zero
-  .byte $7E ; |.XXXXXX.|
-  .byte $72 ; |.XXX..X.|
-  .byte $72 ; |.XXX..X.|
-  .byte $72 ; |.XXX..X.|
-  .byte $7E ; |.XXXXXX.|
-one
-  .byte $1C ; |...XXX..|
-  .byte $1C ; |...XXX..|
-  .byte $1C ; |...XXX..|
-  .byte $1C ; |...XXX..|
-  .byte $1C ; |...XXX..|
-two
-  .byte $7E ; |.XXXXXX.|
-  .byte $40 ; |.X......|
-  .byte $7E ; |.XXXXXX.|
-  .byte $0E ; |....XXX.|
-  .byte $7E ; |.XXXXXX.|
-three
-  .byte $7E ; |.XXXXXX.|
-  .byte $4E ; |.X..XXX.|
-  .byte $1C ; |...XXX..|
-  .byte $4E ; |.X..XXX.|
-  .byte $7E ; |.XXXXXX.|
-four
-  .byte $1C ; |...XXX..|
-  .byte $1C ; |...XXX..|
-  .byte $7E ; |.XXXXXX.|
-  .byte $5C ; |.X.XXX..|
-  .byte $7C ; |.XXXXX..|
-five
-  .byte $7E ; |.XXXXXX.|
-  .byte $0E ; |....XXX.|
-  .byte $7E ; |.XXXXXX.|
-  .byte $40 ; |.X......|
-  .byte $7E ; |.XXXXXX.|
-six
-  .byte $7E ; |.XXXXXX.|
-  .byte $4E ; |.X..XXX.|
-  .byte $7E ; |.XXXXXX.|
-  .byte $40 ; |.X......|
-  .byte $7E ; |.XXXXXX.|
-seven
-  .byte $0E ; |....XXX.|
-  .byte $0E ; |....XXX.|
-  .byte $0E ; |....XXX.|
-  .byte $4E ; |.X..XXX.|
-  .byte $7E ; |.XXXXXX.|
-eight
-  .byte $7E ; |.XXXXXX.|
-  .byte $4E ; |.X..XXX.|
-  .byte $7E ; |.XXXXXX.|
-  .byte $72 ; |.XXX..X.|
-  .byte $7E ; |.XXXXXX.|
-nine
-  .byte $7E ; |.XXXXXX.|
-  .byte $02 ; |......X.|
-  .byte $7E ; |.XXXXXX.|
-  .byte $72 ; |.XXX..X.|
-  .byte $7E ; |.XXXXXX.|
+
+Digits:
+    .BYTE %01111110
+    .BYTE %01110010
+    .BYTE %01110010
+    .BYTE %01110010
+    .BYTE %01111110
+
+    .BYTE %00011100
+    .BYTE %00011100
+    .BYTE %00011100
+    .BYTE %00011100
+    .BYTE %00011100
+
+    .BYTE %01111110
+    .BYTE %00001110
+    .BYTE %01111110
+    .BYTE %01000000
+    .BYTE %01111110
+
+    .BYTE %01111110
+    .BYTE %01001110
+    .BYTE %00011100
+    .BYTE %01001110
+    .BYTE %01111110
+
+    .BYTE %01111100
+    .BYTE %01011100
+    .BYTE %01111110
+    .BYTE %00011100
+    .BYTE %00011100
+
+    .BYTE %01111110
+    .BYTE %01000000
+    .BYTE %01111110
+    .BYTE %00001110
+    .BYTE %01111110
+
+    .BYTE %01111110
+    .BYTE %01000000
+    .BYTE %01111110
+    .BYTE %01001110
+    .BYTE %01111110
+
+    .BYTE %01111110
+    .BYTE %01001110
+    .BYTE %00001110
+    .BYTE %00001110
+    .BYTE %00001110
+
+    .BYTE %01111110
+    .BYTE %01110010
+    .BYTE %01111110
+    .BYTE %01001110
+    .BYTE %01111110
+
+    .BYTE %01111110
+    .BYTE %01110010
+    .BYTE %01111110
+    .BYTE %00000010
+    .BYTE %01111110
 
---
+++ graphics_gen.rb
@@ -0,0 +1,338 @@
+#!/usr/bin/env ruby
+#
+# graphics_gen.rb
+#
+# Builds the graphics.asm file from the template below.
+#
+# Graphic glyphs are built with "X" and spaces, and surrounded on
+# the template with either "!" (X = bit "0") or "|" (X = bit "1"),
+# so we can easily draw tiles, score digits, etc.
+#
+# In either case, glyphs are written upside-down, so we can use
+# (cheaper) decreasing counter loops
+#
+
+@glyphs = <<END
+
+;;;;;;;;;;;;;;
+;; GRAPHICS ;;
+;;;;;;;;;;;;;;
+
+; Tiles are 8 x 11 to partially compensate for
+; Atari's pixel ratio (approx. 1.66:1)
+
+Tiles:
+|XXXXXXXX|
+|XXXXXXXX|
+|XXXXXXXX|
+|XXXXXXXX|
+|XXXXXXXX|
+|XXXXXXXX|
+|XXXXXXXX|
+|XXXXXXXX|
+|XXXXXXXX|
+|XXXXXXXX|
+|XXXXXXXX|
+
+|        |
+| XXXXXX |
+| XXXXXX |
+|     XX |
+| XXXXXX |
+| XXXXXX |
+| XX     |
+| XX     |
+| XXXXXX |
+| XXXXXX |
+|        |
+
+|        |
+| XX  XX |
+| XX  XX |
+| XX  XX |
+| XXXXXX |
+| XXXXXX |
+|     XX |
+|     XX |
+|     XX |
+|     XX |
+|        |
+
+|        |
+|  XXXX  |
+| XX  XX |
+| XX  XX |
+|  XXXX  |
+|  XXXX  |
+| XX  XX |
+| XX  XX |
+| XX  XX |
+|  XXXX  |
+|        |
+
+|        |
+|  X X   |
+|  X X   |
+|  X X   |
+|  X X   |
+|  X XXX |
+|  X X X |
+|  X X X |
+|  X X X |
+|  X XXX |
+|        |
+
+|        |
+|  XX XX |
+|   X  X |
+|   X  X |
+|   X  X |
+|  XX XX |
+|   X X  |
+|   X X  |
+|   X X  |
+|  XX XX |
+|        |
+
+|        |
+| X  X X |
+| X  X X |
+| X  X X |
+| X  XXX |
+| XXX  X |
+| X X  X |
+| X X  X |
+| X X  X |
+| XXX  X |
+|        |
+
+|        |
+|        |
+|X XX XXX|
+|X  X X X|
+|X  X X X|
+|X XX XXX|
+|X X  X X|
+|X X  X X|
+|X XX XXX|
+|        |
+|        |
+
+|        |
+|        |
+|XX XX X|
+| X X  X |
+| X X  X |
+|XX XX XX|
+|X   X XX|
+|X   X XX|
+|XX XX XX|
+|        |
+|        |
+
+|        |
+|        |
+|XX  X XX|
+|X   X  X|
+|X   X  X|
+|XX  X XX|
+| X  X X |
+| X  X X |
+|XX  X XX|
+|        |
+|        |
+
+|   X XXX|
+|   X X X|
+|   X X X|
+|   X X X|
+|   X XXX|
+|        |
+| XXX X X|
+|   X X X|
+| XXX XXX|
+| X     X|
+| XXX   X|
+
+| XXX XXX|
+|   X X X|
+| XXX X X|
+| X   X X|
+| XXX XXX|
+|        |
+| X X XXX|
+| X X X X|
+| XXX XXX|
+|   X X X|
+|   X XXX|
+
+| X X XXX|
+| X X X X|
+| XXX X X|
+|   X X X|
+|   X XXX|
+|        |
+| XXX X  |
+| X X X  |
+| XXX XXX|
+|   X X X|
+|   X XXX|
+
+|  XXX X |
+|  X X X |
+|  XXX X |
+|  X X X |
+|  XXX X |
+|        |
+| XXX XXX|
+| X X   x|
+| XXX XXX|
+|   X X  |
+|   X XXX|
+
+TitleScreenTiles:
+|        |
+|  XXXX  |
+| XX  XX |
+| XX  XX |
+| XX  XX |
+| XX  XX |
+| XX  XX |
+| XX  XX |
+| XX  XX |
+|  XXXX  |
+|        |
+
+|        |
+|  XXXX  |
+| XX  XX |
+| XX     |
+| XXXXX  |
+| XXXXXX |
+| XX  XX |
+| XX  XX |
+| XX  XX |
+|  XXXX  |
+|        |
+
+|        |
+| XX X   |
+| X  XXX |
+| X  X X |
+| XX X X |
+|        |
+|     X  |
+|     XX |
+|     X  |
+|     XX |
+|        |
+
+|        |
+|  X  XX |
+| X X X  |
+| XX   X |
+|  XX XX |
+|        |
+|  X  XX |
+| X X X  |
+| XX  X  |
+|  XX X  |
+|        |
+
+Digits:
+! XXXXXX !
+! XXX  X !
+! XXX  X !
+! XXX  X !
+! XXXXXX !
+
+!   XXX  !
+!   XXX  !
+!   XXX  !
+!   XXX  !
+!   XXX  !
+
+! XXXXXX !
+! X      !
+! XXXXXX !
+!    XXX !
+! XXXXXX !
+
+! XXXXXX !
+! X  XXX !
+!   XXX  !
+! X  XXX !
+! XXXXXX !
+
+!   XXX  !
+!   XXX  !
+! XXXXXX !
+! X XXX  !
+! XXXXX  !
+
+! XXXXXX !
+!    XXX !
+! XXXXXX !
+! X      !
+! XXXXXX !
+
+! XXXXXX !
+! X  XXX !
+! XXXXXX !
+! X      !
+! XXXXXX !
+
+!    XXX !
+!    XXX !
+!    XXX !
+! X  XXX !
+! XXXXXX !
+
+! XXXXXX !
+! X  XXX !
+! XXXXXX !
+! XXX  X !
+! XXXXXX !
+
+! XXXXXX !
+!      X !
+! XXXXXX !
+! XXX  X !
+! XXXXXX !
+
+END
+
+@glyph = []         # Digit or tile being reversed
+@graphics_asm = ""  # Final file contents
+
+def parse_glyphs
+  @glyphs.lines.map(&:rstrip).each do |line|
+    if line.start_with? '|'
+      tile(line)
+    elsif line.start_with? '!'
+      digit(line)
+    else
+      normal(line)
+    end
+  end
+end
+
+def tile(line)
+  @glyph << "    .BYTE %#{line[1..8].tr(' ', '1').tr('^1', '0')}\n"
+end
+
+def digit(line)
+  @glyph << "    .BYTE %#{line[1..8].tr(' ', '0').tr('^0', '1')}\n"
+end
+
+def normal(line)
+  if @glyph.count > 0
+    @graphics_asm << @glyph.push(line).reverse.join
+    @glyph = []
+  end
+
+  @graphics_asm << line << "\n"
+end
+
+parse_glyphs
+File.write 'graphics.asm', @graphics_asm
